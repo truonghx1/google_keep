0fd5ae2038b249191ec68df605906f05fe83baac
truonghuynh <nhokho195@gmail.com>
2026-01-27 10:19:18 +0700

    Add Amplify configuration files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { NavComponent } from './components/sidenav/sidenav.component';
@@ -28,6 +29,7 @@ import { NotesToolsPipe } from './pipes/notes-tools.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
